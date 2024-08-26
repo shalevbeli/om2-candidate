6a8e96dd75026db7ef7c7dbbe73ae3c2d17dfdea
Shalev Zelinger <shalev.zelinger@gmail.com>
2024-08-26 21:21:36 +0300

    Deleted the powershell file

--- examples/complete/second-mission.ps1
+++
@@ -1,7 +0,0 @@
-$computername = ((Get-Content -Path ".\terraform.tfvars" | Select-String -Pattern "instance_name") -split '"')[1];
-Rename-Computer -ComputerName $computername -Force;
-$windowsFeatures = ((Get-Content -Path ".\terraform.tfvars" | Select-String -Pattern "windows-features") -split '"');
-
-foreach ($feature in $windowsFeatures){
-    Install-WindowsFeature -name $feature -IncludeManagementTools;
-}
