fb2ee8aa2d0aebb1969cf5ec4665868966d9a0b1
Shalev Zelinger <shalev.zelinger@gmail.com>
2024-08-27 13:08:30 +0300

    The final section of question 2

--- examples/complete/Powershell-codes/code.ps1
+++ examples/complete/Powershell-codes/code.ps1
@@ -1,3 +1,13 @@
 .\Powershell-codes\question2.ps1
 
-$features = 
+$newName = Read-Host "Enter the new name for the host"
+$username = Read-Host "Enter the username for the server (should be Administrator)"
+$password = Read-Host "Enter the password" -AsSecureString
+$computername = Read-Host "Enter the Public Dns received by aws"
+
+
+$data = Import-Csv -Path "D:\OM2 - Test\om2-candidate\examples\complete\Powershell-codes\Features and users.csv"
+$Features = $data.Features | Where-Object {$_.trim() -ne "" }
+
+Install-Feature -Features $Features -ComputerName $computername -Username $username -password $password
+Rename-ComputerName -ComputerName $computername -NewName $newName -Username $username -password $password
